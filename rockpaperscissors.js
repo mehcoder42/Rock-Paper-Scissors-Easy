8bea22ff9943830979462ab187261844871b2fd2
Mehcoder42 <Mehcoder42@gmail.com>
2020-09-02 21:30:52 +0700

    fixing error and adding xNumberOfGames

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,63 +1,93 @@
 function computerPlay() {
+
     let computerMoves = ['rock', 'paper', 'scissors'];
     computerSelection = computerMoves[Math.floor(Math.random() * computerMoves.length)];
     return computerSelection;
 }
 
 function playerPlay() {
+
     while (playerSelection != 'rock' || playerSelection != 'paper' || playerSelection != 'scissors') {
         playerSelection = window.prompt('Player Score = ' + (playerScore) + '\nComputer Score = ' + (computerScore) + '\nRock, Paper, or Scissors ?');
         playerSelection = playerSelection.toLowerCase();
-        if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') { 
-            return playerSelection; 
+        if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') {
+            return playerSelection;
         }
-        alert('Only Rock, Paper, or Scissors Allowed !!!')
+        alert('Only Rock, Paper, or Scissors Allowed !!!');
     }
 }
 
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log ('You lose! Paper beats Rock');
+        console.log('You lose! Paper beats Rock');
+        alert('You lose! Paper beats Rock');
         return computerScore++
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log ('You lose! Rock beats Scissors');
+        console.log('You lose! Rock beats Scissors');
+        alert('You lose! Rock beats Scissors');
         return computerScore++
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log ('You lose! scissors beats paper');
+        console.log('You lose! scissors beats paper');
+        alert('You lose! scissors beats paper');
         return computerScore++
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        console.log ('You won! Rock beats scissors');
+        console.log('You won! Rock beats scissors');
+        alert('You won! Rock beats scissors');
         return playerScore++
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        console.log ('You won! Scissors beats paper');
+        console.log('You won! Scissors beats paper');
+        alert('You won! Scissors beats paper');
         return playerScore++
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log ('You won! paper beats rock');
+        console.log('You won! paper beats rock');
+        alert('You won! paper beats rock');
         return playerScore++
     } else {
-        console.log ('it\'s a tie ! Looks like both of you pick ' + computerSelection + '.');
+        console.log('it\'s a tie ! Looks like both of you pick ' + computerSelection + '.');
+        alert('it\'s a tie ! Looks like both of you pick ' + computerSelection + '.');
     }
 }
 
-let playerSelection ;
-let computerSelection ;
-let playerScore = 0;
-let computerScore = 0;
+let playerSelection;
+let computerSelection;
+let playerScore;
+let computerScore;
+let x;
+
+function numberOfGames() {
+
+    while (typeof x != 'number') {
+        x = prompt('How many rounds to Play ?');
+        x = Number(x);
+        if (typeof x == 'number') {
+            return x;
+        }
+        alert('Number ONLY !!!');
+    }
+}
+
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        computerPlay(); // getting computer moves
-        playerPlay(); // getting player moves
-        playRound(playerSelection, computerSelection); // play one round RPS
+    
+    
+    playerScore = 0;
+    computerScore = 0;
+    numberOfGames();
+    for (let i = 0; i < x; i++) {
+        computerPlay();
+        playerPlay();
+        playRound(playerSelection, computerSelection);
     }
     if (playerScore > computerScore) {
-        console.log ('You Won ! Congratulations');
+        alert('Winner Winner Chicken Dinner ! Congratulations !');
     } else if (playerScore < computerScore) {
-        console.log ('You Lose to a BOT ! Boo Hoo !!!');
+        alert('No Chicken Dinner For You ! Try Harder Next Time !');
     } else {
-        console.log ('Seems like both of you have the same thoughts huh ?')
+        alert('No Chicken Dinner For Anyone !!!')
     }
+    alert('Type game(); on console to play again !');
+    return ('Type game(); on console to play again !');
 }
 
 game();
